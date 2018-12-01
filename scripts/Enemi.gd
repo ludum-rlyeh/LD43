77cc9e570cb1eb136596bbaea5aefea46b2defbe
riffet jérémy <jeremy.riffet@gmail.com>
2018-12-01 13:28:21 +0100

    ajout signal ennemi arrivée destination (finish_path_signal)

--- scripts/Enemi.gd
+++ scripts/Enemi.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal finish_path_signal
+
 export (float) var speed
 export (float) var SEUIL
 var velocity_norm = Vector2(0,0)
@@ -7,9 +9,7 @@ var path = []
 
 func _process(delta):
 	self.position += self.velocity_norm * self.speed * delta
-#	print(position)
-	update_velocity(delta)
-#	print("enemi ", get_name(), " ", self.velocity_norm, " ", self.speed)
+	update_velocity()
 	
 func set_path(var path):
 	self.path = path.duplicate()
@@ -18,14 +18,15 @@ func set_path(var path):
 func set_speed(var value):
 	self.speed = value
 	
-func update_velocity(var delta):
+func update_velocity():
 	if !self.path.empty():
 		var point = self.path.front() * global.CELL_SIZE
 		var distance = (get_position() - point).length()
 		if distance < SEUIL:
 			self.path.pop_front()
-			update_velocity(delta)
-			return true
+			update_velocity()
 		self.velocity_norm = (point - get_position()).normalized()
+		if self.path.empty():
+			emit_signal("finish_path_signal", self)
 	else:
 		self.velocity_norm = Vector2(0,0)
