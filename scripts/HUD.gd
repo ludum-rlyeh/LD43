513bcc61cbd3eff46bc4c92501d1fac4c1db6cd2
Avzgui <contact.avzgui@gmail.com>
2018-12-01 16:51:51 +0100

    rock hud

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -9,4 +9,9 @@ func _ready():
 
 func update_peon_nb(var nb) :
 	var peon_nb = String(nb / 1000) + String((nb % 1000) / 100) + String((nb % 100) / 10) + String(nb % 10);
-	self.get_node("peons/peon_nb").text = peon_nb
+	self.get_node("VBoxContainer/peons/peon_nb").text = peon_nb
+
+
+func update_rock_nb(var nb) :
+	var rock_nb = String(nb / 1000) + String((nb % 1000) / 100) + String((nb % 100) / 10) + String(nb % 10);
+	self.get_node("VBoxContainer/rocks/rock_nb").text = rock_nb
