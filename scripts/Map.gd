9aa4fa6a2ce53259936c1c605fb20501114f99fe
riffet jérémy <jeremy.riffet@gmail.com>
2018-12-01 15:49:49 +0100

    Ajout matrice de map

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -4,8 +4,10 @@ var paths = [
 	[Vector2(3,0), Vector2(3,4), Vector2(6,4), Vector2(6,7), Vector2(4,7), Vector2(4,9)],
 	[Vector2(12,0), Vector2(12,1), Vector2(10,1), Vector2(10,5), Vector2(6,5), Vector2(6,7), Vector2(4,7), Vector2(4,9)]
 ]
- 
+
 export (PackedScene) var enemi_scene
+
+var matrix = []
 
 func _ready():
 	randomize()
@@ -15,6 +17,24 @@ func _ready():
 		enemi.set_path(get_random_path())
 		enemi.set_speed(rand_range(30, 60))
 		add_child(enemi)
+		
+	var i = 0
+	while($TileMap.get_cell(i, 0) != -1):
+		i += 1
+	self.matrix.resize(i)
+	i = 0
+	while($TileMap.get_cell(0, i) != -1):
+		i += 1
+	for j in range(matrix.size()):
+		matrix[j] = []
+		matrix[j].resize(i)
+		for k in range(matrix[j].size()):
+			if $TileMap.get_cell(j, k) == 9 || $TileMap.get_cell(j, k) == 8:
+				matrix[j][k] = global.PATH_TILE
+			elif $TileMap.get_cell(j, k) == 10 || $TileMap.get_cell(j, k) == 11:
+				matrix[j][k] = global.SOCKET_TILE
+			else :
+				print($TileMap.get_cell(j, k))
 
 func get_path(var index) :
 	if (index <= paths.size()) :
