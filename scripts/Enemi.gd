be1ca56cabf6dc7bbf16e9eddfc8608faf3220cf
riffet jérémy <jeremy.riffet@gmail.com>
2018-12-01 11:23:44 +0100

    Ajout ennemi

--- scripts/Enemi.gd
+++ scripts/Enemi.gd
@@ -1,15 +1,29 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export (float) var SPEED
+export (float) var SEUIL
+var velocity_norm = Vector2(0,0)
+var line = []
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	set_line([Vector2(100,0), Vector2(100,100), Vector2(200,200)])
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	move_and_slide(self.velocity_norm * SPEED)
+	
+	update_velocity(delta)
+	
+func set_line(var line):
+	self.line = line
+	
+func update_velocity(var delta):
+	if !self.line.empty():
+		var point = self.line.front()
+		var distance = (get_position() - point).length()
+		if distance < SEUIL:
+			self.line.pop_front()
+			update_velocity(delta)
+			return true
+		self.velocity_norm = (point - get_position()).normalized()
+	else:
+		self.velocity_norm = Vector2(0,0)
