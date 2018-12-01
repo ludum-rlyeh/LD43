cdc50e469bfa820231ac79d73a0f630472755779
riffet jérémy <jeremy.riffet@gmail.com>
2018-12-01 13:53:29 +0100

    ajout selectionneur

---
+++ scripts/SocketSelectioner.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		move(global.position_to_index(event.position, global.CELL_SIZE))
+	elif event is InputEventMouseButton && event.is_pressed():
+		click_on(global.position_to_index(event.position, global.CELL_SIZE))
+		
+func move(var position_index):
+	self.position = global.index_to_position(position_index, global.CELL_SIZE)
+	
+func click_on(var index):
+	print(index)
