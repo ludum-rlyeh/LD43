79d8a0d04efb5b05c02594c90e896cfd3ab6c005
Avzgui <contact.avzgui@gmail.com>
2018-12-01 17:19:26 +0100

    turret selection

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,8 +1,6 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal turret_selected_signal
 
 func _ready():
 	pass
@@ -15,3 +13,8 @@ func update_peon_nb(var nb) :
 func update_rock_nb(var nb) :
 	var rock_nb = String(nb / 1000) + String((nb % 1000) / 100) + String((nb % 100) / 10) + String(nb % 10);
 	self.get_node("VBoxContainer/rocks/rock_nb").text = rock_nb
+
+func _on_base_turret_gui_input(ev):
+	if (ev is InputEventMouseButton and ev.isPressed()) :
+		var turret = load("res://scenes/Turret.tscn").instance()
+		emit_signal("turret_selected_signal", turret)
