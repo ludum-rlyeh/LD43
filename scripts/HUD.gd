cf8f1afd95b8f6f2e7b4df0e6bf8c636c9685e1c
Avzgui <contact.avzgui@gmail.com>
2018-12-03 21:36:10 +0100

    timer on hud

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -2,6 +2,17 @@ extends Control
 
 signal turret_selected_signal
 
+func _process(delta):
+	var time_left = get_parent().get_parent().get_node("EnemisWavesTimer").time_left
+	if time_left == 0 :
+		print("prout")
+	else :
+		update_timer_label(time_left)
+
+func update_timer_label(var time) :
+	var time_label = String(int(time))
+	self.get_node("Timer").text = time_label
+
 func update_peon_nb(var nb) :
 	var peon_nb = String(nb / 1000) + String((nb % 1000) / 100) + String((nb % 100) / 10) + String(nb % 10)
 	self.get_node("VBoxContainer/peons/peon_nb").text = peon_nb
